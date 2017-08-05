ff594bd5c964dcec401cb091ec2f7945b25d4976
k-mats <gfeenhaftsylphid@gmail.com>
2017-08-05 14:26:13 +0900

    Add `# noqa` marker to suppress pep8 for long urls

--- movie-trailer-website/entertainment_center.py
+++ movie-trailer-website/entertainment_center.py
@@ -5,27 +5,27 @@ import fresh_tomatoes
 # Every poster image comes from Wikimedia
 # Every trailer movie comes from Youtube
 bttf = Movie("Back To The Future",
-             "https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg",
+             "https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg",  # noqa
              "https://www.youtube.com/watch?v=qvsgGtivCgs")
 
 matrix = Movie("The Matrix",
-               "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
+               "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",  # noqa
                "https://www.youtube.com/watch?v=m8e-FF8MsqU")
 
 terminator2 = Movie("Terminator 2",
-                    "https://upload.wikimedia.org/wikipedia/en/8/85/Terminator2poster.jpg",
+                    "https://upload.wikimedia.org/wikipedia/en/8/85/Terminator2poster.jpg",  # noqa
                     "https://www.youtube.com/watch?v=7QXDPzx71jQ")
 
 sw_ep1 = Movie("Star Wars: Episode I",
-               "https://upload.wikimedia.org/wikipedia/en/4/40/Star_Wars_Phantom_Menace_poster.jpg",
+               "https://upload.wikimedia.org/wikipedia/en/4/40/Star_Wars_Phantom_Menace_poster.jpg",  # noqa
                "https://www.youtube.com/watch?v=bD7bpG-zDJQ")
 
 pacific_rim = Movie("Pacific Rim",
-                    "https://upload.wikimedia.org/wikipedia/en/f/f3/Pacific_Rim_FilmPoster.jpeg",
+                    "https://upload.wikimedia.org/wikipedia/en/f/f3/Pacific_Rim_FilmPoster.jpeg",  # noqa
                     "https://www.youtube.com/watch?v=5guMumPFBag")
 
 inception = Movie("Inception",
-                  "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",
+                  "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",  # noqa
                   "https://www.youtube.com/watch?v=8hP9D6kZseM")
 
 # Bundle all movie instances into a list
